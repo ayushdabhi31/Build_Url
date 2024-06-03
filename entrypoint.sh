77f6bb8888a0ffe193ed49933e2e3b4b91f01244
Ayush Dabhi <156417592+ayushdabhi31@users.noreply.github.com>
2024-06-03 14:35:49 +0530

    Add files via upload

---
+++ entrypoint.sh
@@ -0,0 +1,17 @@
+# Get the GitHub Token from GitHub Action inputs
+GITHUB_TOKEN=$1
+
+# Get the pull request number from the GitHub event payload
+pull_request_number=$(jq --raw-output .pull_request.number "$GITHUB_EVENT_PATH")
+echo PR Number - $pull_request_number
+
+build_url=$(echo jq -r '.homepage' package.json)
+
+# Create a comment with the GIF on the pull request
+comment_response=$(curl -sX POST -H "Authorization: token $GITHUB_TOKEN" \
+  -H "Accept: application/vnd.github.v3+json" \
+  -d "{\"body\": \"### PR - #$pull_request_number. \n ### 🎉 Here is your build url! \n ($build_url) \"}" \
+  "https://api.github.com/repos/$GITHUB_REPOSITORY/issues/$pull_request_number/comments")
+
+# Extract and print the comment URL from the comment response
+comment_url=$(echo "$comment_response" | jq --raw-output .html_url)
