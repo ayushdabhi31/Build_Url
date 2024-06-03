efebf3209f3dacf58c28e430a72c114717ad5c91
Ayush Dabhi <156417592+ayushdabhi31@users.noreply.github.com>
2024-06-03 16:12:05 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -27,11 +27,11 @@ if [ -z "$package_json" ]; then
   exit 1
 fi
 
-# Extract the download URL for package.json
-package_json_url=$(echo "$package_json" | jq -r '.download_url')
+# # Extract the download URL for package.json
+# package_json_url=$(echo "$package_json" | jq -r '.download_url')
 
-# Download package.json
-curl -sSL "$package_json_url" -o package.json
+# # Download package.json
+# curl -sSL "$package_json_url" -o package.json
 
 # Extract the homepage URL from package.json
 build_url=$(jq -r '.homepage' package.json)
@@ -42,9 +42,8 @@ if [ -z "$build_url" ]; then
   exit 1
 fi
 
-build_url=$(echo jq -r '.homepage' package.json)
+echo $build_url
 
-# Create a comment with the GIF on the pull request
 comment_response=$(curl -sX POST -H "Authorization: token $GITHUB_TOKEN" \
   -H "Accept: application/vnd.github.v3+json" \
   -d "{\"body\": \"### PR - #$pull_request_number. \n ### 🎉 Here is your build url! \n ($build_url) \"}" \
