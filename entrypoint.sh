e30f72d33a20400f1e149386bcd906929ed61e48
Ayush Dabhi <156417592+ayushdabhi31@users.noreply.github.com>
2024-06-03 16:58:57 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,47 +7,25 @@ GITHUB_TOKEN=$1
 pull_request_number=$(jq --raw-output .pull_request.number "$GITHUB_EVENT_PATH")
 echo PR Number - $pull_request_number
 
-# Fetch repository contents from GitHub
-repo_contents_response=$(curl -sX GET -H "Authorization: token $GITHUB_TOKEN" \
-  -H "Accept: application/vnd.github.v3+json" \
-  "https://api.github.com/repos/$GITHUB_REPOSITORY/contents")
+# Run npm build
+npm run build
 
-# Check if the repository contents were fetched successfully
-if [ "$(echo "$repo_contents_response" | jq -r '.message')" = "Not Found" ]; then
-  echo "Error: Unable to fetch repository contents"
-  exit 1
-fi
+# Zip the build folder
+zip -r build.zip build
 
-# Find package.json in the repository contents
-package_json=$(echo "$repo_contents_response" | jq -r '.[] | select(.name == "package.json")')
+# Upload the zip file to GitHub Releases
+upload_url=$(curl -sH "Authorization: token $GITHUB_TOKEN" \
+  -H "Content-Type: application/zip" \
+  --upload-file build.zip \
+  "https://api.github.com/repos/$GITHUB_REPOSITORY/releases/latest/assets?name=build.zip" | jq -r '.browser_download_url')
 
-# Check if package.json was found
-if [ -z "$package_json" ]; then
-  echo "Error: package.json file not found in the repository"
-  exit 1
-fi
-
-# # Extract the download URL for package.json
-# package_json_url=$(echo "$package_json" | jq -r '.download_url')
-
-# # Download package.json
-# curl -sSL "$package_json_url" -o package.json
-
-# Extract the homepage URL from package.json
-build_url=$(jq -r '.homepage' package.json)
-
-# Check if build_url is empty
-if [ -z "$build_url" ]; then
-  echo "Error: homepage URL not found in package.json"
-  exit 1
-fi
-
-echo $build_url
-
+# Post a comment on the pull request with the link to the zip file
 comment_response=$(curl -sX POST -H "Authorization: token $GITHUB_TOKEN" \
   -H "Accept: application/vnd.github.v3+json" \
-  -d "{\"body\": \"### PR - #$pull_request_number. \n ### 🎉 Here is your build url! \n ($build_url) \"}" \
+  -d "{\"body\": \"### PR - #$pull_request_number. \n ### 🎉 Here is your build zip file! \n [Download Build Zip]($upload_url) \"}" \
   "https://api.github.com/repos/$GITHUB_REPOSITORY/issues/$pull_request_number/comments")
 
 # Extract and print the comment URL from the comment response
 comment_url=$(echo "$comment_response" | jq --raw-output .html_url)
+
+echo "Comment posted at: $comment_url"
