4c0c320bb39d23b8f2564f5861ab7889db5b9f5e
Ayush Dabhi <156417592+ayushdabhi31@users.noreply.github.com>
2024-06-04 22:50:32 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,19 @@
+### entrypoint.sh
+
+bash
+#!/bin/bash
+
+set -e
+
+# Navigate to the repository
+cd "$GITHUB_WORKSPACE"
+
+# Run the build command
+npm install
+npm run build
+
+# Zip the build directory
+zip -r build.zip build
 
 # Get the pull request number
 PR_NUMBER=$(jq --raw-output .pull_request.number "$GITHUB_EVENT_PATH")
@@ -36,11 +52,10 @@ if [ -z "$download_url" ]; then
   exit 1
 fi
 
-
 # Post a comment on the pull request with the link to the zip file
 comment_response=$(curl -sX POST -H "Authorization: token $GITHUB_TOKEN" \
   -H "Accept: application/vnd.github.v3+json" \
-  -d "{\"body\": \"### PR - #$PR_NUMBER. \n ### 🎉 Here is your build zip file! \n [Download Build Zip]($DOWNLOAD_URL) \"}" \
+  -d "{\"body\": \"### PR - #$PR_NUMBER. \n ### 🎉 Here is your build zip file! \n [Download Build Zip]($download_url) \"}" \
   "https://api.github.com/repos/$GITHUB_REPOSITORY/issues/$PR_NUMBER/comments")
 
 # Extract and print the comment URL from the comment response
@@ -53,6 +68,3 @@ if [ -z "$comment_url" ]; then
 fi
 
 echo "Comment posted at: $comment_url"
-          
-          
-
